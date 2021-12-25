bd02ddb3b01fbb9ec2f3695f1e45deea0df49d71
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-12-25 20:16:55 +0530

    upload project starter

---
+++ lib/screens/signup.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 250,
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
