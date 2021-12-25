320b4de5ce9a4ee0b445ee3ee2f0d94145364bff
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-12-25 20:48:29 +0530

    Signup UI part 1

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -5,14 +5,111 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
+    return Scaffold(
+      body: Container(
         child: Column(
           children: <Widget>[
             Container(
-              height: 250,
-              color: Colors.blue,
-            )
+              height: 220,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text("Register", style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),)
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+
+            Container(
+              height: 400,
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "USERNAME",
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                        },
+                        child: Icon(Icons.visibility, color: Colors.black,),
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.black
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "USERNAME",
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                        },
+                        child: Icon(Icons.visibility, color: Colors.black,),
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.black
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "USERNAME",
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                        },
+                        child: Icon(Icons.visibility, color: Colors.black,),
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.black
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "USERNAME",
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                        },
+                        child: Icon(Icons.visibility, color: Colors.black,),
+                      ),
+                      hintStyle: TextStyle(
+                        color: Colors.black
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+
+                  Container(
+                    height: 45,
+                    width: double.infinity,
+                    child: RaisedButton(
+                      child: Text("Register"),
+                      color: Colors.blueGrey[400],
+                      onPressed: () {}),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
